fd23fd1bcd96ff7fe3f892ad9c04394adc10610b
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-27 01:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const panels = document.querySelectorAll(".card");
+
+    panels.forEach(card => {
+        const para = card.querySelector(".img-text");
+        para.style.display = card.classList.contains("active") ? "block" : "none";
+
+	});
+
+	const imgs=document.querySelectorAll(".img");
+
+	imgs.forEach(img=>{
+		img.addEventListener("click", function(){
+			let activeImg=document.querySelector(".active");
+			activeImg.classList.remove("active");
+			let card=img.parentElement;
+			card.classList.add("active")
+		panels.forEach(card => {
+        const para = card.querySelector(".img-text");
+        para.style.display = card.classList.contains("active") ? "block" : "none";
+
+	});
+		})
+	});
+});
